04abac050cce1ee5801a3b4672b952058dcd81e6
Masanori Ohgita <mp@ohgita.info>
2016-05-13 03:24:48 +0900

    Add: Basic認証

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').Server(app);
- 
+
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({limit: '5mb'}));
+
+// Basic認証 - e.g. BASIC_AUTH='taro:0123,hanako:4567'
+var basicAuth = require('basic-auth-connect'), basicAuthUsers = {};
+if (process.env['BASIC_AUTH']) {
+	process.env['BASIC_AUTH'].split(/,/).forEach(function (chunk, i) {
+		var chunk_ = chunk.split(/:/);
+		basicAuthUsers[chunk_[0]] = chunk_[1];
+	});
+}
+console.log(Object.keys(basicAuthUsers).length);
+app.use(basicAuth(function (req_user, req_pw) {
+	return ((Object.keys(basicAuthUsers).length == 0) || (basicAuthUsers[req_user] && basicAuthUsers[req_user] === req_pw));
+}));
 
 // wwwディレクトリを静的ファイルディレクトリとして登録
 app.use(express.static('www'));
@@ -29,23 +42,23 @@ app.get(/(\d+)\/pde\/([A-Z]{2}\d{2}[A-Z]\d{3})\.pde/, function(req, res){
 		console.log("error");
 		res.send("e");
 	}
-	
-	
+
+
 });
 
 app.get(/(\d+)\/studentList.js/, function(req, res){
 	try{
 		var m = req.url.match(/(\d+)\/studentList.js/);
 		var no = m[1];
-		
+
 		var studentList = new Array();
 		for ( var id in pdeListArray[no] ) {
-		
+
 			studentList.push({ id:pdeListArray[no][id]["userId"],name:pdeListArray[no][id]["userName"]});
 		}
-		
+
 		res.send("var studentList = " + JSON.stringify(studentList) + ";")
-	
+
 	}catch(e){
 		console.log("error");
 		res.send("e");
@@ -56,9 +69,9 @@ app.get(/(\d+)\/studentList.js/, function(req, res){
 app.post('/pdeList', function(req, res){
 
 	pdeListArray.push(req.body);
-	
+
 	app.use('/'+(pdeListArray.length-1),express.static('viewer'));
-	
+
 	res.writeHead(200, { "Content-Type": "text/plain" });
 	res.write(""+(pdeListArray.length-1));
 	res.end();
